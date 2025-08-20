ff7839b4b172560378daed872475c29ca19cbdb9
AbhayPandy <abhaypandey370003@gmail.com>
2025-08-20 21:38:46 +0530

    Hyperparameter training

--- src/utils.py
+++ src/utils.py
@@ -22,37 +22,45 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models: dict, params: dict):
     try:
         report = {}
 
-        for name, model in models.items():
+        for model_name, model in models.items():
+            param_grid = params.get(model_name, {})  # safely get params
+
+            if param_grid:  # run GridSearch if params available
+                gs = GridSearchCV(model, param_grid, cv=3, n_jobs=-1)
+                gs.fit(X_train, y_train)
+                model.set_params(**gs.best_params_)
+            
+            # fit final model
             model.fit(X_train, y_train)
 
+            # predictions
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
 
+            # scores
             train_score = r2_score(y_train, y_train_pred)
             test_score = r2_score(y_test, y_test_pred)
 
-            report[name] = {
+            # log both train & test scores
+            report[model_name] = {
                 "train_score": train_score,
-                "test_score": test_score,
+                "test_score": test_score
             }
 
-            # print(f"{name}: Train R2={train_score:.4f}, Test R2={test_score:.4f}")
-            
         return report
 
     except Exception as e:
         raise CustomException(e, sys)
 
     
-# def load_object(file_path):
-#     try:
-#         with open(file_path, "rb") as file_obj:
-#             return pickle.load(file_obj)
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
-#     except Exception as e:
-#         raise CustomException(e, sys)
-    
+    except Exception as e:
+        raise CustomException(e, sys)
